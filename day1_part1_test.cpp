daac4fe6d6db4138ffd23544fa6934b88af9bcc6
Ben Goodwyn <bengoodwyn@me.com>
2019-12-09 00:37:47 -0600

    :bug: Fix tests broken when removing old headers

--- day1_part1_test.cpp
+++ day1_part1_test.cpp
@@ -1,15 +1,15 @@
 #include <catch2/catch.hpp>
 
-#include "day1_part1.hpp"
+#include "day1.hpp"
 
 TEST_CASE("A module of mass 14 requires 2 fuel", "[day1]") {
-  REQUIRE(2 == fuel_required(14));
+  REQUIRE(2 == fuel_required_for_mass(14));
 }
 
 TEST_CASE("For a mass of 1969, the fuel required is 654", "[day1]") {
-  REQUIRE(654 == fuel_required(1969));
+  REQUIRE(654 == fuel_required_for_mass(1969));
 }
 
 TEST_CASE("For a mass of 100756, the fuel required is 33583", "[day1]") {
-  REQUIRE(33583 == fuel_required(100756));
+  REQUIRE(33583 == fuel_required_for_mass(100756));
 }
